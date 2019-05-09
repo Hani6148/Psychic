3dc5ac0bf1f56b1e50b3f2e46074afe0b1b21963
deadpool <hanikhellafi@gmail.com>
2019-05-09 18:56:53 -0500

    version of 5/9/2019

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,75 +1,69 @@
+// a win counter for the number of wins
+// a lose counter for the number of losses
+// a left counter for the number of tries left
+// 
 var win=0;
 var lose=0;
+// initializing counters:
 var left=9;
+
+var alf=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t",
+"u","v","w","x","y","z"];
+document.getElementById("win").innerHTML= "win "+win;
+document.getElementById("lose").innerHTML= "lose "+lose;
+document.getElementById("far").innerHTML="your guesses so far: ";
+
+
 document.onkeyup=function(){
+   
     var compGuess = "";
 
-     var ran = Math.floor(Math.random() *  9);
+     var ran = Math.floor(Math.random() *  26);
      console.log(ran);
-     if(ran==0){
-         compGuess="a";
-
-     }
-     if(ran==1){
-        compGuess="b";
-
-    }
-    if(ran==2){
-        compGuess="c";
-    }
-    if(ran==3){
-        compGuess="d";
-
-    }
-    if(ran==4){
-        compGuess="e";
-
-    }
-    if(ran==5){
-        compGuess="f";
-
-    }
-    if(ran==6){
-        compGuess="g";
-
-    }
-    if(ran==7){
-        compGuess="h";
-
-    }
-    if(ran==8){
-        compGuess="i";
-
-    }
-    if(ran==9){
-        compGuess="j";
-
-    }
-    console.log(compGuess);
+     compGuess=alf[ran];
+     console.log(compGuess);
 
 
      var guess = event.key;
      console.log(guess);
 
+     document.getElementById("far").innerHTML+=guess;
+     left--;
+
+
      if(guess==compGuess){
-         alert("win");
+        
          win++;
+         
+         alert("winyou won, you winning letter is "+guess);
          left=9;
+
+         document.getElementById("far").innerHTML="your guesses so far: ";
+        
          
      }
-     else{
-         
-         left--;
-     }
+     
      if(left==0)
      {
-        alert("lose");
+        
         lose++;
+
+        
+        
+        alert("you lost");
         left=9;
+        document.getElementById("far").innerHTML="your guesses so far: ";
+       
      }
+     
+     
      document.getElementById("win").innerHTML= "win "+win;
      document.getElementById("lose").innerHTML= "lose "+lose;
      document.getElementById("left").innerHTML= "guesses left  "+left;
+
+     
+     
+
 
 
 }
